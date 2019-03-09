57352bd1a023fefd5b406c9f867700defe99d095
Sanjeet Dasharath <u6138440@anu.edu.au>
2019-03-09 13:34:00 +0530

    complete 1 - blueprints

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,13 +1,32 @@
-from flask import Flask, jsonify
+import os
 
-app = Flask(__name__)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+# instantiate the db
+db = SQLAlchemy()
+
+
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extentions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+    
+    return app
